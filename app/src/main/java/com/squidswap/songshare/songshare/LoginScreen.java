0e3d4e6b473b2180097c89036cf33e1fa07ebdea
ddcjkinghorn <john.kinghorn@coxautoinc.com>
2018-12-14 16:41:15 -0500

    update

---
+++ app/src/main/java/com/squidswap/songshare/songshare/LoginScreen.java
@@ -0,0 +1,18 @@
+package com.squidswap.songshare.songshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.login_screen);
+    }
+}
