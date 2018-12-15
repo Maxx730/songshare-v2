dbcc6b5a69e17f9c69c6ce55f8e91722cdc6e973
Maxx730 <max.kinghorn@gmail.com>
2018-12-15 15:45:37 -0500

    update

---
+++ app/src/main/java/UiServices/TrackFragment.java
@@ -0,0 +1,24 @@
+package UiServices;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.squidswap.songshare.songshare.R;
+
+public class TrackFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.track_fragment, container, false);
+    }
+}
---
+++ app/src/main/java/UiServices/TrackPageAdapter.java
@@ -0,0 +1,21 @@
+package UiServices;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TrackPageAdapter extends FragmentPagerAdapter {
+    public TrackPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
