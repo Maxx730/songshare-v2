bd3d10b3ffec713b867a024a0143e7ccf3693886
Maxx730 <max.kinghorn@gmail.com>
2018-12-15 14:52:57 -0500

    update

--- app/src/main/java/UiServices/StreamPageAdapter.java
+++ app/src/main/java/UiServices/StreamPageAdapter.java
@@ -3,6 +3,7 @@ package UiServices;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
 
 public class StreamPageAdapter extends FragmentPagerAdapter {
     public StreamPageAdapter(FragmentManager fm) {
@@ -11,6 +12,7 @@ public class StreamPageAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int i) {
+        Log.d("STREAM PAGE ADAPTER","Current Page: " +  String.valueOf(i));
         return null;
     }
 
